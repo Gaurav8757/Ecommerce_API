e6587ebc46b4c1926740a3d301776e04b32c2d96
Ritik <ritikpatel705@gmail.com>
2023-09-24 21:16:47 +0530

    update routes

--- routes/api/product_api.js
+++ routes/api/product_api.js
@@ -1,7 +1,7 @@
 // Importing Express
 const express = require('express');
 //Importing the Product controller to redirect to perform the desired function
-const productController = require('../../controllers/api/v1/productController');
+const productController = require('../../controllers/api/productController');
 // console.log(productController);
 // Creating a router to redirect requests
 const router = express.Router();
